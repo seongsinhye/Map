631eab4f566bc5d32f528a6a52eff86c16fc9815
seongsinhye <sinhye2428@naver.com>
2025-01-26 18:18:38 +0900

    init---

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManger = CLLocationManager()
+    @IBOutlet var myMap: MKMapView!
+    
+    @IBOutlet var lbLocationInfo1: UILabel!
+    
+    @IBOutlet var lbLocationInfo2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lbLocationInfo1.text = ""
+        lbLocationInfo2.text = ""
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.startUpdatingLocation()
+        myMap.showsUserLocation = true
+        
     }
 
-
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
+    
 }
 
